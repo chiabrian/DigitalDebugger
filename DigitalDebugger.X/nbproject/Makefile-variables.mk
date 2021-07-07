341798e195f472cc49136a2f004e5c47e2bc6f63
brian <chia.brian@gmail.com>
2021-07-06 22:36:02 -0400

    --Initial Commit --PCB ordered from Oshpark

---
+++ DigitalDebugger.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DigitalDebugger.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DigitalDebugger.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=digitaldebugger.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/digitaldebugger.x.tar
